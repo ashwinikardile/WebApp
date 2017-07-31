22bf5d59fb24a1308ddac05a8c86c31cfb0fa1c1
ashwinikardile <ashwinikardile3@gmail.com>
2017-07-31 16:02:12 -0500

    Model code changed

---
+++ ContactWeb/ContactWeb/Models/Contact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContactWeb.Models
+{
+	public class Contact
+	{
+		public int Id { get; set; }
+		public Guid UserId { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Email { get; set; }
+		public DateTime birthday { get; set; }
+		public string contactPrim { get; set; }
+		public string contactSec { get; set; }
+		public string streetAddress1 { get; set; }
+		public string streetAddress2 { get; set; }
+		public string city { get; set; }
+		public string State { get; set; }
+		public string Zip { get; set; }
+
+	}
+}
